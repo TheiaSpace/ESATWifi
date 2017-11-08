a209cb26a322ea284b50566de773720cd6034669
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2017-11-08 14:00:17 +0100

    Document the main program.

--- examples/Wifi/Wifi.ino
+++ examples/Wifi/Wifi.ino
@@ -1,6 +1,9 @@
 #include "ESAT_Wifi.h"
 #include <ESAT_CCSDSPacket.h>
 
+// Main program of the Wifi board: respond to telecommands and send
+// telemetry.
+
 const byte NETWORK_CONNECTION_TIMEOUT_SECONDS = 60;
 const word PACKET_DATA_BUFFER_LENGTH = 256;
 const word WHOLE_PACKET_BUFFER_LENGTH =
@@ -9,6 +12,7 @@ const word WHOLE_PACKET_BUFFER_LENGTH =
 byte radioBuffer[WHOLE_PACKET_BUFFER_LENGTH];
 byte serialBuffer[WHOLE_PACKET_BUFFER_LENGTH];
 
+// Start the peripherals and do some bookkeeping work.
 void setup()
 {
   (void) WiFi.disconnect(true);
@@ -21,6 +25,15 @@ void setup()
                   NETWORK_CONNECTION_TIMEOUT_SECONDS);
 }
 
+// Body of the main loop of the program:
+// - Try to read a packet from the radio interface.
+//   - If there is a telecommand packet, send it to the On-Board
+//     Computer through the serial interface.
+// - Try to read a packet from the serial interface.
+//   - If there is a telecommand packet, handle it.
+//   - If there is a telemetry packet, send it through the radio
+//     interface.
+// - Update the state of the module.
 void loop()
 {
   byte packetDataBuffer[PACKET_DATA_BUFFER_LENGTH];
